f0ba673e53e48a4e81783049c6f35be3fadaaf3d
Aaron <aaronklinker1@gmail.com>
2026-01-16 10:21:02 -0600

    chore: Move builders file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+/**
+ * ## Error Handling
+ *
+ * Inside any of your handlers, just throw an Error and the library will handle the rest.
+ *
+ * @module
+ */
+
 export * from "./createMcpFetchTransport";
 export * from "./defineMcpPrompt";
 export * from "./defineMcpResource";
 export * from "./defineMcpResourceTemplate";
 export * from "./defineMcpTool";
 export * from "./types";
-export * from "./result-builder";
+export * from "./result-builders";
